b251d792a8a826f5984a68f17f392460651ec261
Estefania Ortiz <estefaniaortizvelazquez@ymail.com>
2012-11-10 00:44:17 -0800

    physics class start spaceship class made

---
+++ Planets/Physics.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Planets
+{
+    class Physics
+    {
+        public static float getNetForce(Spaceship ship, List<Planet> planets){
+            float totalForce=0;
+            foreach (Planet p in planets){
+                   
+            }
+            return totalForce;
+        }
+
+
+    }
+}
