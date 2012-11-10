ebe8c66bd9036c8065ad029a0b2ecfa398cb662a
Casey Wojcik <jarlaxle180@gmail.com>
2012-11-10 03:28:38 -0800

    no

--- Planets/Planets/Game.cs
+++ Planets/Planets/Game.cs
@@ -16,11 +16,12 @@ namespace Planets
         {
             w = new World();
             w.setSpaceship(new Spaceship(new Vector(200, 0)));
+            for (int i = 0; i < 10; i++) w.addPlanet(new Planet());
         }
         public void step(object s, object ev)
         {
             w.step();
-            Graphics.drawWorld(w);
+            Graphics.update(w);
         }
 
         public void setup()
@@ -28,6 +29,7 @@ namespace Planets
             timer.Tick += new EventHandler<object>(step);
             timer.Interval = new TimeSpan(0, 0, 0, 0, 100);
             timer.Start();
+            Graphics.drawWorld(w);
         }
         public World getWorld()
         {
