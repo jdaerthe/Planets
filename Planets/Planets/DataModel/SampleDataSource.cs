73bd9b8a05e7dde0cc68380324a12fc04e30373e
Estefania Ortiz <estefaniaortizvelazquez@ymail.com>
2012-11-10 14:30:17 -0800

    numbers

--- Planets/Planets/DataModel/SampleDataSource.cs
+++ Planets/Planets/DataModel/SampleDataSource.cs
@@ -285,7 +285,7 @@ namespace Planets.Data
             var gettingStarted = new SampleDataItem("start",
                 "Getting Started",
                 "How to play?",
-                "image", "",
+                "Assets/one.png", "",
                 "Your rocket will be affected by the gravitational pull of the planets arround it. To start you must press anywhere on the screen, this controls how fast and in which direction the rocket will initially travel.",
                 group3);
             group3.Items.Add(gettingStarted);
@@ -293,7 +293,7 @@ namespace Planets.Data
             var objective = new SampleDataItem("objective",
                 "Objective",
                 "How do you win in arcade mode?",
-                "image", "",
+                "Assets/two.png", "",
                 "In order to win the game you must get your spaceship back to Earth without getting trapped by the gravitational pull of other planets.",
                 group3);
 
@@ -302,7 +302,7 @@ namespace Planets.Data
             var free = new SampleDataItem("free",
                 "Free Mode",
                 "Experimenting with gravity.",
-                "image", "",
+                "Assets/three.png", "",
                 "In the Free Mode you have the power to control the planet amount, gravitational pull, and relative sizes of planets to see how planets interact with the spaceship.",
                 group3);
             group3.Items.Add(free);
