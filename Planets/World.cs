6dc10f397f36879b922fcbc136d3c8cf4c7867f4
Casey Wojcik <jarlaxle180@gmail.com>
2012-11-10 00:43:37 -0800

    second

---
+++ Planets/World.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planets
+{
+    class World
+    {
+        private Spaceship spaceship;
+        private List<Planet> planets;
+        
+        public World() {
+            spaceship = new Spaceship();
+            planets = new List<Planet>();
+            Physics.getNetForce(spaceship, planets);
+        }
+
+        public void addPlanet(Planet p)
+        {
+            planets.Add(p);
+        }
+    }
+}
