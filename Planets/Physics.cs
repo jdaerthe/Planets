1253d89ebc6c60f172570b4ff7eaa4b8566152c3
Estefania Ortiz <estefaniaortizvelazquez@ymail.com>
2012-11-10 01:12:34 -0800

    helper funct physics

--- Planets/Physics.cs
+++ Planets/Physics.cs
@@ -14,11 +14,19 @@ namespace Planets
             float totalForce=0;
             foreach (Planet p in planets){
                 float currForce = 0;
-                float gravity = p.getGravity();
-                float distance = Math.Sqrt(Math.Pow(p.getX() - ship.getX(), 2) + Math.Pow(p.getY() - ship.getY(), 2));
+                float gravity = forceOfGravity(ship, p);
+                
 
             }
             return totalForce;
+        }
+
+        public static float forceOfGravity(Spaceship s,Planet p){
+            return (p.getGravity() * p.getMass() * s.getMass())/(getDistance(s,p));
+        }
+
+        private float getDistance(Spaceship s, Planet p){
+            return Math.Sqrt(Math.Pow(p.getX() - s.getX(), 2) + Math.Pow(p.getY() - s.getY(), 2));
         }
 
 
