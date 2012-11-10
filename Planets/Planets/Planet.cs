fa3efb428370ab814f912d822e3fabc5c56d7e5a
Casey Wojcik <jarlaxle180@gmail.com>
2012-11-10 01:51:09 -0800

    Implemented planet

--- Planets/Planets/Planet.cs
+++ Planets/Planets/Planet.cs
@@ -8,6 +8,48 @@ namespace Planets
 {
     class Planet
     {
-        
+        private Vector position;
+        private float mass;
+
+        public Planet()
+        {
+
+        }
+        public Planet(Vector p)
+        {
+            position = p;
+        }
+        public void setPosition(Vector p)
+        {
+            position = p;
+        }
+        public Vector getPosition()
+        {
+            return position;
+        }
+        public void setX(int x)
+        {
+            this.position.x = x;
+        }
+        public void setY(int y)
+        {
+            this.position.y = y;
+        }
+        public float getX()
+        {
+            return position.x;
+        }
+        public float getY()
+        {
+            return position.y;
+        }
+        public void setMass(float m)
+        {
+            mass = m;
+        }
+        public float getMass()
+        {
+            return mass;
+        }
     }
 }
